49c05936f3786ce566aaa65aa3da0291f7dee540
Jim Bevington <jim.bevington@hotmail.com>
2018-03-19 16:38:39 +0000

    added crude twoNames() method to NameRandomiser

--- src/main/java/NameRandomiser.java
+++ src/main/java/NameRandomiser.java
@@ -4,6 +4,8 @@ import java.util.Random;
 public class NameRandomiser {
 
     private ArrayList<String> names;
+    private int range;
+    private Random rand;
 
     public NameRandomiser() {
         this.names = new ArrayList<String>();
@@ -21,14 +23,35 @@ public class NameRandomiser {
         names.add("Gary");
         names.add("Josef");
         names.add("Bratislav III");
+
+        this.range = this.names.size();
+
+        this.rand = new Random();
     }
 
 
     public String oneName() {
-        int range = this.names.size();
-        Random rand = new Random();
-        return this.names.get(rand.nextInt(range));
+//        Random rand = new Random();
+        return this.names.get(rand.nextInt(this.range));
     }
 
 
+    public ArrayList<String> twoNames() {
+
+        ArrayList<String> twoNames = new ArrayList<String>();
+
+        String name1 = this.names.get(rand.nextInt(range));
+        String name2 = this.names.get(rand.nextInt(range));
+
+//        until both names are different, regenerate the 2nd name
+        while (name1 == name2) {
+            name2 = this.names.get(rand.nextInt(range));
+        }
+
+        twoNames.add(name1);
+        twoNames.add(name2);
+
+        return twoNames;
+
+    }
 }
