a3f854cb5c6f39c061d4194987abc9c28c047066
Jim Bevington <jim.bevington@hotmail.com>
2018-03-19 16:48:00 +0000

    get one name route to Controller, not checked yet

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,7 +1,12 @@
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import javax.naming.Name;
+
+import java.util.HashMap;
+
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class Controller {
 
@@ -10,6 +15,19 @@ public class Controller {
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
         staticFileLocation("/public");
+
+        get("/name_randomiser/1", (req, res) -> {
+
+            NameRandomiser nameRandomiser = new NameRandomiser();
+            String name = nameRandomiser.oneName();
+
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("name", name);
+
+            model.put("template", "one_name.vtl");
+            return new ModelAndView(model, "template.vtl");
+
+        }, velocityTemplateEngine);
 
     }
 }
