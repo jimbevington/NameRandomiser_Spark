67254ddae7b73c93421f979c8df7c65a18b96351
Jim Bevington <jim.bevington@hotmail.com>
2018-03-19 20:32:51 +0000

    added new route to handle any number of names

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.sun.tools.internal.xjc.model.Model;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -20,30 +22,50 @@ public class Controller {
 
         NameRandomiser nameRandomiser = new NameRandomiser();
 
-        get("/name_randomiser/1", (req, res) -> {
 
-            String name = nameRandomiser.oneName();
+        get("/name_randomiser/:number", (req, res) -> {
 
-            HashMap<String, Object> model = new HashMap<>();
-            model.put("name", name);
+            int numNames = Integer.parseInt(req.params(":number"));
 
-            model.put("template", "one_name.vtl");
-            return new ModelAndView(model, "layout.vtl");
-
-        }, velocityTemplateEngine);
-
-
-        get("/name_randomiser/2", (req, res) -> {
-
-            ArrayList<String> names = nameRandomiser.twoNames();
+            ArrayList<String> names = nameRandomiser.getRandomNames(numNames);
 
             HashMap<String, Object> model = new HashMap<>();
             model.put("names", names);
 
-            model.put("template", "two_names.vtl");
+            model.put("template", "names_list.vtl");
             return new ModelAndView(model, "layout.vtl");
 
         }, velocityTemplateEngine);
+
+
+
+//        ORIGINAL HARD-CODED NUMBER OF NAMES
+
+//        get("/name_randomiser/1", (req, res) -> {
+//
+//            String name = nameRandomiser.oneName();
+//
+//            HashMap<String, Object> model = new HashMap<>();
+//            model.put("name", name);
+//
+//            model.put("template", "one_name.vtl");
+//            return new ModelAndView(model, "layout.vtl");
+//
+//        }, velocityTemplateEngine);
+//
+//
+//        get("/name_randomiser/2", (req, res) -> {
+//
+//            ArrayList<String> names = nameRandomiser.twoNames();
+//
+//            HashMap<String, Object> model = new HashMap<>();
+//            model.put("names", names);
+//
+//            model.put("template", "two_names.vtl");
+//            return new ModelAndView(model, "layout.vtl");
+//
+//        }, velocityTemplateEngine);
+
 
     }
 }
